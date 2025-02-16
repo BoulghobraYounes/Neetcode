f11ae6531c559b17227a47c73fc8d6ec732b964b
BoulghobraYounes <younes.boulghobrapro@gmail.com>
2025-02-16 18:00:42 +0100

    Solved the containg duplicate problem

---
+++ Arrays & Hashing/contains_duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False   
+         
+solution = Solution()
+myList = [7, 8, 2, 6, 15]
+myList2 = [19, 25, 26, 99, 25]
+print(solution.hasDuplicate(myList))
+print(solution.hasDuplicate(myList2))
