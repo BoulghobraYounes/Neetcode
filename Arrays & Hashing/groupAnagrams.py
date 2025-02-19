6ac0d59c94cb02971dfb48f0630e42ef5d9c0c82
BoulghobraYounes <younes.boulghobrapro@gmail.com>
2025-02-19 20:28:59 +0100

    optimized solution for the group anagrams problem

--- Arrays & Hashing/groupAnagrams.py
+++ Arrays & Hashing/groupAnagrams.py
@@ -1,32 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        results = []
-        index = 0
-        for s in range(len(strs)):
-            if any(strs[s] in sublist for sublist in results):
-                continue
-            results.append([strs[s]])
-            for t in range(s + 1, len(strs)):
-                if self.isAnagram(strs[s], strs[t]):
-                    results[index].append(strs[t])
-            index += 1       
-        return results 
-
-    
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0) 
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countT == countS 
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
 
 
 strs = ["", "", "act","pots","tops","cat","stop","hat"]
 solution = Solution()
-print(solution.groupAnagrams(strs))
-print(solution.isAnagram("", ""))            
+print(solution.groupAnagrams(strs))      
 
 
